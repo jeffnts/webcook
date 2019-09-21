c8fa9ff587aaab02e207ba36daad1fe22b889e28
jeffnts <jf.nts10@gmail.com>
2019-09-20 22:24:55 -0300

    CMS - Database change to MongoDB

--- packages/cms/config/functions/bootstrap.js
+++ packages/cms/config/functions/bootstrap.js
@@ -10,4 +10,6 @@
  * See more details here: https://strapi.io/documentation/3.0.0-beta.x/configurations/configurations.html#bootstrap
  */
 
+if(process.env.NODE_ENV !== 'production') require('dotenv').config();
+
 module.exports = () => {};
